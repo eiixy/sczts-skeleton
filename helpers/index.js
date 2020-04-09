ca7696f607eb33eb1c522646dd3c6af10c7a4d49
eiixy <990656271@qq.com>
2020-04-09 14:00:46 +0800

    增加防抖与节流函数

--- helpers/index.js
+++ helpers/index.js
@@ -6,6 +6,47 @@ import obj from "./Obj"
 import random from "./Random"
 import str from "./Str"
 
+
+/**
+ * 防抖
+ * @param {*} fn 
+ * @param {*} delay
+ */
+function debounce(fn, delay) {
+    let timer = null
+    return function () {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(fn, delay)
+    }
+}
+
+/**
+ * 节流
+ * @param {*} fn 
+ * @param {*} delay 
+ */
+function throttle(fn, delay) {
+    let last_time
+    let timer = null
+    return function () {
+        let cur_time = new Date().getTime()
+        if (last_time && cur_time < last_time + delay) { //若为真，则表示上次执行过，且在期限值范围内
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                fn();
+                last_time = cur_time
+            }, delay)
+        } else {
+            last_time = cur_time;
+            fn();
+        }
+    }
+}
+
+
+
 export const Arr = arr;
 export const Cache = cache;
 export const Cookie = cookie;
@@ -13,14 +54,17 @@ export const Date = date;
 export const Obj = obj;
 export const Random = random;
 export const Str = str;
-
+export const Debounce = debounce
+export const Throttle = throttle
 
 export default {
-    Arr:arr,
-    Cache:cache,
-    Cookie:cookie,
-    Date:date,
-    Obj:obj,
-    Random:random,
-    Str:str
+    Arr: arr,
+    Cache: cache,
+    Cookie: cookie,
+    Date: date,
+    Debounce: debounce,
+    Obj: obj,
+    Random: random,
+    Str: str,
+    Throttle: throttle,
 }
